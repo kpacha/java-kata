ca0b889321769268c3219bfad67d5374fd4f0015
Kpacha <kpacha666@gmail.com>
2013-06-15 23:17:49 +0200

    fixing the test for the 5

--- src/main/java/com/github/kpacha/jkata/fizzBuzz/FizzBuzz.java
+++ src/main/java/com/github/kpacha/jkata/fizzBuzz/FizzBuzz.java
@@ -1,12 +1,13 @@
 package com.github.kpacha.jkata.fizzBuzz;
 
-
 public class FizzBuzz {
 
     public static Object generate(int total) {
 	Object answer = null;
 	if (total == 3) {
 	    answer = "Fizz";
+	} else if (total == 5) {
+	    answer = "Buzz";
 	} else {
 	    answer = total;
 	}
