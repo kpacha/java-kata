a2ee4fc33485477956a66a419f36e5a7f81010b8
Kpacha <kpacha666@gmail.com>
2013-06-15 21:45:24 +0200

    adding a new test

--- src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
+++ src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
@@ -12,6 +12,10 @@ public class PrimeFactorsTest extends TestCase {
 	assertEquals(list(), PrimeFactors.generate(1));
     }
 
+    public void testTwo() throws Exception {
+	assertEquals(list(2), PrimeFactors.generate(2));
+    }
+
     private List<Integer> list() {
 	return new ArrayList<Integer>();
     }
