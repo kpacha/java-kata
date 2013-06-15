9101c390c938576bee1b58e37d892cb44e303992
Kpacha <kpacha666@gmail.com>
2013-06-15 23:07:15 +0200

    generalization of the test

--- src/test/java/com/github/kpacha/jkata/fizzBuzz/test/FizzBuzzTest.java
+++ src/test/java/com/github/kpacha/jkata/fizzBuzz/test/FizzBuzzTest.java
@@ -1,18 +1,36 @@
 package com.github.kpacha.jkata.fizzBuzz.test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 import com.github.kpacha.jkata.fizzBuzz.FizzBuzz;
 
+@RunWith(Parameterized.class)
 public class FizzBuzzTest extends TestCase {
 
-    public void testOne() {
-	assertEquals(Arrays.asList(1), FizzBuzz.generate(1));
+    private List<Object> list;
+    private int total;
+
+    public FizzBuzzTest(int total, List<Object> list) {
+	this.total = total;
+	this.list = list;
     }
 
-    public void testTwo() {
-	assertEquals(Arrays.asList(1, 2), FizzBuzz.generate(2));
+    @Test
+    public void testGenerator() {
+	assertEquals(list, FizzBuzz.generate(total));
+    }
+
+    @Parameterized.Parameters
+    public static List<Object[]> data() {
+	final Object[][] objects = { { 1, Arrays.asList(1) },
+		{ 2, Arrays.asList(1, 2) } };
+	return Arrays.asList(objects);
     }
 }
