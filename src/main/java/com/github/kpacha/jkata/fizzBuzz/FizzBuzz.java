403f71d8671fc28dc983432913653e2aedc76ed0
Kpacha <kpacha666@gmail.com>
2013-06-15 23:24:23 +0200

    fixing the test for the 15 (FizzBuzz)

--- src/main/java/com/github/kpacha/jkata/fizzBuzz/FizzBuzz.java
+++ src/main/java/com/github/kpacha/jkata/fizzBuzz/FizzBuzz.java
@@ -3,12 +3,14 @@ package com.github.kpacha.jkata.fizzBuzz;
 public class FizzBuzz {
 
     public static Object generate(int total) {
-	Object answer = null;
+	Object answer = "";
 	if (total % 3 == 0) {
 	    answer = "Fizz";
-	} else if (total % 5 == 0) {
-	    answer = "Buzz";
-	} else {
+	}
+	if (total % 5 == 0) {
+	    answer += "Buzz";
+	}
+	if (answer.equals("")) {
 	    answer = total;
 	}
 	return answer;
