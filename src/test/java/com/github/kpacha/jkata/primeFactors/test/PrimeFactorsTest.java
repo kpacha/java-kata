89bf939092be70463a75a6ed0cfb07c3dfeffa77
Kpacha <kpacha666@gmail.com>
2013-06-15 22:00:16 +0200

    testing the number 8 (with numbers 5, 6 & 7 just works fine)

--- src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
+++ src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
@@ -24,6 +24,22 @@ public class PrimeFactorsTest extends TestCase {
 	assertEquals(list(2, 2), PrimeFactors.generate(4));
     }
 
+    public void testFive() throws Exception {
+	assertEquals(list(5), PrimeFactors.generate(5));
+    }
+
+    public void testSix() throws Exception {
+	assertEquals(list(2, 3), PrimeFactors.generate(6));
+    }
+
+    public void testSeven() throws Exception {
+	assertEquals(list(7), PrimeFactors.generate(7));
+    }
+
+    public void testEight() throws Exception {
+	assertEquals(list(2, 2, 2), PrimeFactors.generate(8));
+    }
+
     private List<Integer> list(int... ints) {
 	List<Integer> list = new ArrayList<Integer>();
 	for (int i : ints) {
