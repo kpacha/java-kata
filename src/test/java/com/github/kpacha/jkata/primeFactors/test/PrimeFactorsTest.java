adcd04f0b85f2ade4646b670bb4f2e25eb41848b
Kpacha <kpacha666@gmail.com>
2013-06-15 21:48:01 +0200

    extending the 'data provider' for the new requirement

--- src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
+++ src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
@@ -16,7 +16,11 @@ public class PrimeFactorsTest extends TestCase {
 	assertEquals(list(2), PrimeFactors.generate(2));
     }
 
-    private List<Integer> list() {
-	return new ArrayList<Integer>();
+    private List<Integer> list(int... ints) {
+	List<Integer> list = new ArrayList<Integer>();
+	for (int i : ints) {
+	    list.add(i);
+	}
+	return list;
     }
 }
