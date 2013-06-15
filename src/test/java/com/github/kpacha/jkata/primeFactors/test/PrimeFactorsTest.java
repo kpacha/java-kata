595f28ae1e5f86845fa0c807903a224fbe6dd9d4
Kpacha <kpacha666@gmail.com>
2013-06-15 21:36:57 +0200

    adding private helper signature

--- src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
+++ src/test/java/com/github/kpacha/jkata/primeFactors/test/PrimeFactorsTest.java
@@ -1,9 +1,15 @@
 package com.github.kpacha.jkata.primeFactors.test;
 
+import java.util.List;
+
 import junit.framework.TestCase;
 
 public class PrimeFactorsTest extends TestCase {
     public void testOne() throws Exception {
 	assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    private List<Integer> list() {
+	return null;
     }
 }
