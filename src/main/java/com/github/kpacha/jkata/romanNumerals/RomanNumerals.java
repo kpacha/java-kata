a3a705a1cd8667f078df77677392ab4edd437ab4
Kpacha <kpacha666@gmail.com>
2013-06-16 13:40:30 +0200

    implementation refactor

--- src/main/java/com/github/kpacha/jkata/romanNumerals/RomanNumerals.java
+++ src/main/java/com/github/kpacha/jkata/romanNumerals/RomanNumerals.java
@@ -3,12 +3,11 @@ package com.github.kpacha.jkata.romanNumerals;
 public class RomanNumerals {
 
     public static String convert(int number) {
-	if (number == 3) {
-	    return "III";
+	String roman = "";
+	while (number >= 1) {
+	    roman += "I";
+	    number--;
 	}
-	if (number == 2) {
-	    return "II";
-	}
-	return "I";
+	return roman;
     }
 }
