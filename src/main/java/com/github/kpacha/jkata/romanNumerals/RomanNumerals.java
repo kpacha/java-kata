a62cad3a6a51e84de7b1ada483b9b9056051fc2b
Kpacha <kpacha666@gmail.com>
2013-06-16 14:20:57 +0200

    fixing the test 500

--- src/main/java/com/github/kpacha/jkata/romanNumerals/RomanNumerals.java
+++ src/main/java/com/github/kpacha/jkata/romanNumerals/RomanNumerals.java
@@ -7,6 +7,7 @@ public class RomanNumerals {
 
     private static List<RomanArabicPair> eqivalencies = new ArrayList<RomanArabicPair>() {
 	{
+	    add(new RomanArabicPair("D", 500));
 	    add(new RomanArabicPair("CD", 400));
 	    add(new RomanArabicPair("C", 100));
 	    add(new RomanArabicPair("XC", 90));
