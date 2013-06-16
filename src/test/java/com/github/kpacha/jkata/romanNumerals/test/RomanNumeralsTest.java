4454efbe35fe66fbdf411904dad351735647d662
Kpacha <kpacha666@gmail.com>
2013-06-16 14:10:39 +0200

    testing the number 10

--- src/test/java/com/github/kpacha/jkata/romanNumerals/test/RomanNumeralsTest.java
+++ src/test/java/com/github/kpacha/jkata/romanNumerals/test/RomanNumeralsTest.java
@@ -31,7 +31,7 @@ public class RomanNumeralsTest extends TestCase {
     public static List<Object[]> data() {
 	final Object[][] objects = { { 1, "I" }, { 2, "II" }, { 3, "III" },
 		{ 4, "IV" }, { 5, "V" }, { 6, "VI" }, { 7, "VII" },
-		{ 8, "VIII" }, { 9, "IX" } };
+		{ 8, "VIII" }, { 9, "IX" }, { 10, "X" } };
 	return Arrays.asList(objects);
     }
 }
