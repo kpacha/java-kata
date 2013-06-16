8fee5085223c3bb583f1a4fcf8a1dc54d3f5d514
Kpacha <kpacha666@gmail.com>
2013-06-16 14:20:22 +0200

    testing the number 500

--- src/test/java/com/github/kpacha/jkata/romanNumerals/test/RomanNumeralsTest.java
+++ src/test/java/com/github/kpacha/jkata/romanNumerals/test/RomanNumeralsTest.java
@@ -34,7 +34,7 @@ public class RomanNumeralsTest extends TestCase {
 		{ 8, "VIII" }, { 9, "IX" }, { 10, "X" }, { 14, "XIV" },
 		{ 19, "XIX" }, { 21, "XXI" }, { 40, "XL" }, { 50, "L" },
 		{ 59, "LIX" }, { 66, "LXVI" }, { 80, "LXXX" }, { 90, "XC" },
-		{ 100, "C" }, { 400, "CD" } };
+		{ 100, "C" }, { 400, "CD" }, { 500, "D" } };
 	return Arrays.asList(objects);
     }
 }
