b9be4a5b73158b9b77e8733d9e628b95ec772f36
Kpacha <kpacha666@gmail.com>
2013-06-21 20:31:38 +0200

    Refactoring the scenario in order to prepare the last step of the current movement

--- src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -1,8 +1,13 @@
 package com.github.kpacha.jkata.anagram;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Anagram {
 
-    public static String generate(String source) {
-	return source;
+    public static Set<String> generate(String source) {
+	Set<String> result = new HashSet<String>();
+	result.add(source);
+	return result;
     }
 }
--- src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
+++ src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
@@ -10,7 +10,7 @@ import com.github.kpacha.jkata.anagram.Anagram;
 public class AnagramTest extends TestCase {
 
     public void testSingleLetterWord() {
-	assertEquals("I", Anagram.generate("I"));
+	assertEquals(buildSet("I"), Anagram.generate("I"));
     }
 
     public void testTwoLetterWord() {
