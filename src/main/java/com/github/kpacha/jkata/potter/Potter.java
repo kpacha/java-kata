c03f77fbfc41b88e8e2117119e3577c92e971323
Kpacha <kpacha666@gmail.com>
2013-06-21 19:13:42 +0200

    moving away the discount calculation

--- src/main/java/com/github/kpacha/jkata/potter/Potter.java
+++ src/main/java/com/github/kpacha/jkata/potter/Potter.java
@@ -3,12 +3,14 @@ package com.github.kpacha.jkata.potter;
 public class Potter {
 
     public static double priceFor(int items) {
+	return 8 * items * getDiscountFor(items);
+    }
+
+    private static double getDiscountFor(int items) {
 	if (items == 3)
-	    return 8 * 3 * 0.90;
+	    return 0.9;
 	if (items == 2)
-	    return 8 * 2 * 0.95;
-	if (items == 1)
-	    return 8;
-	return 0;
+	    return 0.95;
+	return 1.0;
     }
 }
