6a8126bcfd0cd357ac71596abfa5ac8104ba76c2
Kpacha <kpacha666@gmail.com>
2013-06-21 20:36:50 +0200

    fixing the test for two letters

--- src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -7,6 +7,9 @@ public class Anagram {
 
     public static Set<String> generate(String source) {
 	Set<String> result = new HashSet<String>();
+	if (source.length() == 2) {
+	    result.add(source.substring(1) + source.substring(0, 1));
+	}
 	result.add(source);
 	return result;
     }
