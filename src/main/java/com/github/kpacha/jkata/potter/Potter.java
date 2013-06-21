1fd21c0342e93ef212ea8f989eef1c28468a3abf
Kpacha <kpacha666@gmail.com>
2013-06-21 19:28:39 +0200

    fixing the 'two full collections' test

--- src/main/java/com/github/kpacha/jkata/potter/Potter.java
+++ src/main/java/com/github/kpacha/jkata/potter/Potter.java
@@ -17,9 +17,10 @@ public class Potter {
     };
 
     public static double priceFor(int... items) {
+	double total = 8 * items[0] * getDiscountFor(items[0]);
 	if (items.length == 2)
-	    return 8 * items[0] * getDiscountFor(items[0]) + 8;
-	return 8 * items[0] * getDiscountFor(items[0]);
+	    total += 8 * items[1] * getDiscountFor(items[1]);
+	return total;
     }
 
     private static double getDiscountFor(int items) {
