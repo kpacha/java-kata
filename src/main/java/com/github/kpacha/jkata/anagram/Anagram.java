74ec2356b3414e3d81040ecd65f12435d42e4fc6
Kpacha <kpacha666@gmail.com>
2013-06-21 20:44:30 +0200

    fixing the test for three letters

--- src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -7,6 +7,18 @@ public class Anagram {
 
     public static Set<String> generate(String source) {
 	Set<String> result = new HashSet<String>();
+	if (source.length() == 3) {
+	    result.add(source.substring(0, 1) + source.substring(1));
+	    result.add(source.substring(0, 1) + source.substring(2)
+		    + source.substring(1, 2));
+	    result.add(source.substring(1, 2) + source.substring(0, 1)
+		    + source.substring(2));
+	    result.add(source.substring(1, 2) + source.substring(2)
+		    + source.substring(0, 1));
+	    result.add(source.substring(2) + source.substring(0, 2));
+	    result.add(source.substring(2) + source.substring(1, 2)
+		    + source.substring(0, 1));
+	}
 	if (source.length() == 2) {
 	    result.add(source.substring(1) + source.substring(0, 1));
 	}
