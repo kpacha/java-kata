6f65003d5f1631f28f512d2381dea98a70e64011
Kpacha <kpacha666@gmail.com>
2013-06-21 19:48:57 +0200

    Fixing the single letter test

---
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -0,0 +1,8 @@
+package com.github.kpacha.jkata.anagram;
+
+public class Anagram {
+
+    public static String generate(String source) {
+	return source;
+    }
+}
--- src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
+++ src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
@@ -2,6 +2,8 @@ package com.github.kpacha.jkata.anagram.test;
 
 import junit.framework.TestCase;
 
+import com.github.kpacha.jkata.anagram.Anagram;
+
 public class AnagramTest extends TestCase {
 
     public void testSingleLetterWord() {
