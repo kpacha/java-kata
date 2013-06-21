5d877ff0b93b0f025fb1e5848b2766e4c4836ec4
Kpacha <kpacha666@gmail.com>
2013-06-21 20:30:29 +0200

    Implementing the set builder for the tests

--- src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
+++ src/test/java/com/github/kpacha/jkata/anagram/test/AnagramTest.java
@@ -1,5 +1,8 @@
 package com.github.kpacha.jkata.anagram.test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
 
 import com.github.kpacha.jkata.anagram.Anagram;
@@ -13,4 +16,13 @@ public class AnagramTest extends TestCase {
     public void testTwoLetterWord() {
 	assertEquals(buildSet("on", "no"), Anagram.generate("no"));
     }
+
+    private Set<String> buildSet(String... words) {
+	Set<String> result = new HashSet<String>();
+	for (String word : words) {
+	    result.add(word);
+	}
+	return result;
+    }
+
 }
