3a764148c61ee521a9b493cef80b8f3873f91489
Kpacha <kpacha666@gmail.com>
2013-06-21 19:02:32 +0200

    fixing the empty order test

--- src/test/java/com/github/kpacha/jkata/potter/test/PotterTest.java
+++ src/test/java/com/github/kpacha/jkata/potter/test/PotterTest.java
@@ -2,6 +2,8 @@ package com.github.kpacha.jkata.potter.test;
 
 import junit.framework.TestCase;
 
+import com.github.kpacha.jkata.potter.Potter;
+
 public class PotterTest extends TestCase {
 
     public void testEmptyOrder() {
