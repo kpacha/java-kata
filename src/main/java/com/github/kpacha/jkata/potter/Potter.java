37785133aae6e8c84439e939964fdc22a7656659
Kpacha <kpacha666@gmail.com>
2013-06-21 19:18:01 +0200

    creating the discount map

--- src/main/java/com/github/kpacha/jkata/potter/Potter.java
+++ src/main/java/com/github/kpacha/jkata/potter/Potter.java
@@ -1,16 +1,24 @@
 package com.github.kpacha.jkata.potter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Potter {
+
+    private static Map<Integer, Double> discount = new HashMap<Integer, Double>() {
+	{
+	    put(3, 0.9);
+	    put(2, 0.95);
+	    put(1, 1.0);
+	    put(0, 1.0);
+	}
+    };
 
     public static double priceFor(int items) {
 	return 8 * items * getDiscountFor(items);
     }
 
     private static double getDiscountFor(int items) {
-	if (items == 3)
-	    return 0.9;
-	if (items == 2)
-	    return 0.95;
-	return 1.0;
+	return discount.get(items);
     }
 }
