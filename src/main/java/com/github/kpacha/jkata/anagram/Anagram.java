ad0469d576df2c7b4cfb260fd65972698356012e
Kpacha <kpacha666@gmail.com>
2013-06-21 21:41:32 +0200

    getting the string content as an array of characters

--- src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -1,13 +1,20 @@
 package com.github.kpacha.jkata.anagram;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Anagram {
 
     public static Set<String> generate(String source) {
 	Set<String> result = new HashSet<String>();
-	if (source.length() == 3) {
+	List<Character> chars = new ArrayList<Character>();
+	for (char c : source.toCharArray()) {
+	    chars.add(c);
+	}
+
+	if (chars.size() == 3) {
 	    for (String part : Anagram.generate(source.substring(1))) {
 		result.add(source.substring(0, 1) + part);
 	    }
@@ -19,7 +26,7 @@ public class Anagram {
 		result.add(source.substring(2) + part);
 	    }
 	}
-	if (source.length() == 2) {
+	if (chars.size() == 2) {
 	    for (String part : Anagram.generate(source.substring(0, 1))) {
 		result.add(source.substring(1) + part);
 	    }
@@ -27,7 +34,7 @@ public class Anagram {
 		result.add(source.substring(0, 1) + part);
 	    }
 	}
-	if (source.length() == 1) {
+	if (chars.size() == 1) {
 	    result.add(source);
 	}
 	return result;
