2869f7466abac032d4426a5ef80bc50f340f39a7
Kpacha <kpacha666@gmail.com>
2013-06-21 20:47:55 +0200

    refactoring the three letters anagram generation

--- src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
+++ src/main/java/com/github/kpacha/jkata/anagram/Anagram.java
@@ -8,16 +8,16 @@ public class Anagram {
     public static Set<String> generate(String source) {
 	Set<String> result = new HashSet<String>();
 	if (source.length() == 3) {
-	    result.add(source.substring(0, 1) + source.substring(1));
-	    result.add(source.substring(0, 1) + source.substring(2)
-		    + source.substring(1, 2));
-	    result.add(source.substring(1, 2) + source.substring(0, 1)
-		    + source.substring(2));
-	    result.add(source.substring(1, 2) + source.substring(2)
-		    + source.substring(0, 1));
-	    result.add(source.substring(2) + source.substring(0, 2));
-	    result.add(source.substring(2) + source.substring(1, 2)
-		    + source.substring(0, 1));
+	    for (String part : Anagram.generate(source.substring(1))) {
+		result.add(source.substring(0, 1) + part);
+	    }
+	    for (String part : Anagram.generate(source.substring(0, 1)
+		    + source.substring(2))) {
+		result.add(source.substring(1, 2) + part);
+	    }
+	    for (String part : Anagram.generate(source.substring(0, 2))) {
+		result.add(source.substring(2) + part);
+	    }
 	}
 	if (source.length() == 2) {
 	    result.add(source.substring(1) + source.substring(0, 1));
