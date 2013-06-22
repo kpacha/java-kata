9dadd060f18525f39bcf1dc6a741d81f936509aa
Kpacha <kpacha666@gmail.com>
2013-06-22 17:49:26 +0200

    testing other card numeric value

--- src/test/java/com/github/kpacha/jkata/pokerhand/test/PokerHandTest.java
+++ src/test/java/com/github/kpacha/jkata/pokerhand/test/PokerHandTest.java
@@ -9,8 +9,14 @@ import com.github.kpacha.jkata.pokerhand.PokerCard;
 public class PokerHandTest extends TestCase {
 
     @Test
-    public void testGetNumericValue() {
+    public void testGetFiveAsNumericValue() {
 	PokerCard card = new PokerCard("5H");
 	assertEquals(5, card.getNumericValue());
+    }
+
+    @Test
+    public void testGetNineAsNumericValue() {
+	PokerCard card = new PokerCard("9H");
+	assertEquals(9, card.getNumericValue());
     }
 }
