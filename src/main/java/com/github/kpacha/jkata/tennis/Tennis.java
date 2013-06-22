95adbc098df6702f212c0e4adef64f0d53fc141b
Kpacha <kpacha666@gmail.com>
2013-06-22 15:18:40 +0200

    fixing the 'third advantage' test

--- src/main/java/com/github/kpacha/jkata/tennis/Tennis.java
+++ src/main/java/com/github/kpacha/jkata/tennis/Tennis.java
@@ -8,15 +8,21 @@ public class Tennis {
     public String getScore() {
 	if (isDeuce())
 	    return "Deuce";
-	if (isAdvantage())
+	if (isAdvantagePlayerOne())
 	    return "Advantage Player 1";
+	if (isAdvantagePlayerTwo())
+	    return "Advantage Player 2";
 	if (playerOneScored == 4)
 	    return "Player 1 wins";
 	return (15 * playerOneScored) + " - " + (15 * playerTwoScored);
     }
 
-    private boolean isAdvantage() {
+    private boolean isAdvantagePlayerOne() {
 	return playerOneScored == playerTwoScored + 1 && playerOneScored > 3;
+    }
+
+    private boolean isAdvantagePlayerTwo() {
+	return playerOneScored + 1 == playerTwoScored && playerTwoScored > 3;
     }
 
     private boolean isDeuce() {
