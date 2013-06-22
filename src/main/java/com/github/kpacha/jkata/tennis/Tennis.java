0631858548bba8fe7bfc0727eee3698e227332f9
Kpacha <kpacha666@gmail.com>
2013-06-22 14:58:05 +0200

    fixing the advantage test

--- src/main/java/com/github/kpacha/jkata/tennis/Tennis.java
+++ src/main/java/com/github/kpacha/jkata/tennis/Tennis.java
@@ -8,8 +8,12 @@ public class Tennis {
     public String getScore() {
 	if (playerOneScored == 3 && playerTwoScored == 3)
 	    return "Deuce";
-	if (playerOneScored == 4)
-	    return "Player 1 wins";
+	if (playerOneScored == 4) {
+	    if (playerTwoScored == 3)
+		return "Advantage Player 1";
+	    else
+		return "Player 1 wins";
+	}
 	return (15 * playerOneScored) + " - " + (15 * playerTwoScored);
     }
 
